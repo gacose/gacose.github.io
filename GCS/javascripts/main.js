4fa5b949ab814452b6c6e4792c83f43ddec9c82a
gacose <gacose@gmail.com>
2016-08-25 14:05:17 -0500

    pruebas

--- GCS/javascripts/main.js
+++ GCS/javascripts/main.js
@@ -1,3 +1,9 @@
+var adicion=function(a,b){
+  return(a+b)
+}
+
+
+
 var sectionHeight = function() {
   var total    = $(window).height(),
       $section = $('section').css('height','auto');
@@ -13,22 +19,42 @@ var sectionHeight = function() {
 $(window).resize(sectionHeight);
 
 $(document).ready(function(){
+
+$('#resultado').append("<h2>el resultado de la suma de 3 y 6 es</h2>" + adicion(3,6)+"<br>");
+
+
+var suma=0
+for (var i=0; i<100; i++){
+  suma=suma+i;
+}
+  $('#resultado').append( suma+ "<br>");
+
+
+  var mult=1
+  for (var i=1; i<10; i++){
+    mult=mult*i;
+    $('#resultado').append( mult + "<br>");
+  }
+
+
+
+
   $("section h1, section h2").each(function(){
     $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
     $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
     $("nav ul li:first-child a").parent().addClass("active");
   });
-  
+
   $("nav ul li").on("click", "a", function(event) {
     var position = $($(this).attr("href")).offset().top - 190;
     $("html, body").animate({scrollTop: position}, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
-    event.preventDefault();    
+    event.preventDefault();
   });
-  
+
   sectionHeight();
-  
+
   $('img').load(sectionHeight);
 });
 
